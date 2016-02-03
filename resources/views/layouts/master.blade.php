b522324a61c0129238d71456b6b9253eeaac07b0
laurenfazah <laurenfazah@gmail.com>
2016-02-03 14:51:35 -0500

    created and migrated over to laravel install

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <title>Screenshotter</title>
+
+        <meta name="twitter:card" content="">
+        <meta name="twitter:site" content="">
+        <meta name="twitter:title" content="">
+        <meta name="twitter:description" content="">
+        <meta name="twitter:image:src" content="">
+
+        <meta property="og:title" content=""/>
+        <meta property="og:url" content=""/>
+        <meta property="og:image" content=""/>
+        <meta property="og:site_name" content=""/>
+        <meta property="og:description" content=""/>
+
+        <link rel="stylesheet" href="stylesheets/style.css">
+
+        <script src="js/vendor/modernizr.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <script>
+            // google analytics
+        </script>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="/js/vendor/jquery.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
