bcb348b1df0730f856f42c9e4e7e61637250eac8
laurenfazah <laurenfazah@gmail.com>
2016-02-09 12:20:38 -0500

    cleaned up comments in screenshots controller

--- app/Http/Controllers/ScreenshotsController.php
+++ app/Http/Controllers/ScreenshotsController.php
@@ -53,32 +53,23 @@ class ScreenshotsController extends Controller
     public function crawlSite($site, $filepath)
     {
 
-        $crawler = new PHPCrawler();
+        $crawler = new PHPCrawler();                                // set new instance
 
-        // URL to crawl
-        $crawler->setURL($site);
+        $crawler->setURL($site);                                    // URL to crawl
 
-        // $crawler->setWorkingDirectory($filepath);
+        // $crawler->setWorkingDirectory($filepath);                   // optional change from CD
 
-        // Only receive content of files with content-type "text/html"
-        $crawler->addContentTypeReceiveRule("#text/html#");
+        $crawler->addContentTypeReceiveRule("#text/html#");         // only receive content-type "text/html"
 
-        // Ignore links to pictures, dont even request pictures
-        $crawler->addURLFilterRule("#\.(jpg|jpeg|gif|png)$# i");
+        $crawler->addURLFilterRule("#\.(jpg|jpeg|gif|png)$# i");    // ignore & don't request pics
 
-        // Store and send cookie-data like a browser does
-        $crawler->enableCookieHandling(true);
+        $crawler->enableCookieHandling(true);                       // store and send cookie-data like a browser does
 
-        // Set the traffic-limit to 1 MB (in bytes,
-        // for testing we dont want to "suck" the whole site)
-        // $crawler->setTrafficLimit(1000 * 1024);
+        // $crawler->setTrafficLimit(1000 * 1024);                  // limiting traffic (for dev)
 
-        // Thats enough, now here we go
-        $crawler->go();
+        $crawler->go();                                             // all info in, good to go
 
-        // At the end, after the process is finished, we print a short
-        // report (see method getProcessReport() for more information)
-        $report = $crawler->getProcessReport();
+        $report = $crawler->getProcessReport();                     // optional report
 
         if (PHP_SAPI == "cli") $lb = "\n";
         else $lb = "<br />";
@@ -89,7 +80,7 @@ class ScreenshotsController extends Controller
         echo "Bytes received: ".$report->bytes_received." bytes".$lb;
         echo "Process runtime: ".$report->process_runtime." sec".$lb;
 
-        $this->takeScreenshots($data, $filepath);
+        $this->takeScreenshots($data, $filepath);                   // follow through to take screenshots
 
     }
 
