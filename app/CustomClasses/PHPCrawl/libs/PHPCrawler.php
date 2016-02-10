2bd8bea2efd6cef85f1541aa92bc2d19a4148f80
laurenfazah <laurenfazah@gmail.com>
2016-02-10 17:19:54 -0500

    removed status messages during site scan

--- app/CustomClasses/PHPCrawl/libs/PHPCrawler.php
+++ app/CustomClasses/PHPCrawl/libs/PHPCrawler.php
@@ -1124,24 +1124,25 @@ class PHPCrawler
 
   public function handleDocumentInfo($DocInfo){
     // Just detect linebreak for output ("\n" in CLI-mode, otherwise "<br>").
-    if (PHP_SAPI == "cli") $lb = "\n";
-    else $lb = "<br />";
+    // if (PHP_SAPI == "cli") $lb = "\n";
+    // else $lb = "<br />";
     // Print the URL and the HTTP-status-Code
-    echo "Page requested: ".$DocInfo->url." (".$DocInfo->http_status_code.")".$lb;
+    // echo "Page requested: ".$DocInfo->url." (".$DocInfo->http_status_code.")".$lb;
 
     // Print the refering URL
-    echo "Referer-page: ".$DocInfo->referer_url.$lb;
+    // echo "Referer-page: ".$DocInfo->referer_url.$lb;
 
     // Print if the content of the document was be recieved or not
-    if ($DocInfo->received == true)
-      echo "Content received: ".$DocInfo->bytes_received." bytes".$lb;
-    else
-      echo "Content not received".$lb;
+    // if ($DocInfo->received == true)
+      // echo "Content received: ".$DocInfo->bytes_received." bytes".$lb;
+    // else
+      // echo "Content not received".$lb;
 
     // Now you should do something with the content of the actual
     // received page or file ($DocInfo->source), we skip it in this example
 
-    echo $lb;
+    // echo $lb;
+
 
     $data = array();
     $data["url"] = $DocInfo->url;
